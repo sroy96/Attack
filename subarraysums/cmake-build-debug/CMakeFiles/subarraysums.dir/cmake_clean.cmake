f2546436a3a9f1c93ac965b16895218500c959ff
saurav-roy <saurav_roy@apple.com>
2020-05-11 00:10:23 +0530

    intervies

---
+++ subarraysums/cmake-build-debug/CMakeFiles/subarraysums.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subarraysums.dir/main.cpp.o"
+  "subarraysums"
+  "subarraysums.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subarraysums.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
