81ce64cca6cb035c827da0839e1a9d60904a5514
saurav-roy <saurav_roy@apple.com>
2020-05-11 00:34:02 +0530

    max sum in subarray

--- subarraysums/main.cpp
+++ subarraysums/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <vector>
+using  namespace std;
+
+
+void func(vector<int>vec,int k){
+    int i=0;
+    int j=0;
+    int temp=0;
+    while(i<vec.size()){
+        temp=temp+vec[j];
+        if(temp==k){
+            break;
+        }
+        else if(temp<k){
+            j++;
+        }
+        else if(temp>k){
+            i++;
+            j=i;
+            temp=0;
+        }
+    }
+    cout<<i+1<<" "<<j+1;
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int t;
+    cin>>t;
+    while(t--){
+        int size,sum;
+        cin>>size>>sum;
+        vector<int>vec;
+        for(int i=0;i<size;i++){
+            int x;
+            cin>>x;
+            vec.push_back(x);
+        }
+        func(vec,sum);
+
+    }
 }
