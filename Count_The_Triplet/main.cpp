691f2f3b0c69eefbe58f391b9cfdf0a855482e0a
saurav-roy <saurav_roy@apple.com>
2020-05-13 01:27:31 +0530

    reverse links to solve

--- Count_The_Triplet/main.cpp
+++ Count_The_Triplet/main.cpp
@@ -1,37 +1,20 @@
-
-/**
- * Given an array of distinct integers. The task is to count all the triplets
- * such that sum of two elements equals the third element.
- * first+second = third;
- */
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void count_triplet(vector<int>vec,int n){
-    sort(vec.begin(),vec.end());
-    int f,s,t,count=0;
-    for( f=0;f<n-2;f++){
-        s=f+1;
-        t=s+1;
-        while(t<n){
-            if(vec[f]+vec[s]==vec[t]){
-                count++;
-                s++;
-            }
-            else if (vec[f]+vec[s]<vec[t]){
-                t--;
-                s++;
-            }
-
-            t++;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n ;
+        cin>>n;
+        int ac=( n * (n+1))/2;
+        int c=0;
+        for(int i=1;i<n;i++){
+            int x;
+            cin>>x;
+           c=c+x;
         }
+        cout<<ac-c<<endl;
     }
-    cout<<count<<endl;
-}
-
-int main(){
-    vector<int>vec{1,2,3,5,7};
-    count_triplet(vec,5);
     return 0;
 }
---
+++ Reverse_LinkedList/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+/**
+ * Node current = head;
+Node prev = null;
+
+while(current!=null){
+head = current.next;
+current.next = prev;
+prev = current;
+current = head;
+}
+return prev;
+ */
+
+int main()
+{
+    std::cout<<"Hello, World!"<<std::endl;
+    return 0;
+}
